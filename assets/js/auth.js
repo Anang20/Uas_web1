e4943885fa72a1783e82f2731f16f1c0d4770f34
anangsyah <anangsyah766@gmail.com>
2024-12-30 18:40:29 +0700

    feat: add login page

---
+++ assets/js/auth.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Mencegah pengiriman form secara default
+    
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+    
+        // Validasi input
+        if (email === '' || password === '') {
+            alert('Silakan isi semua field!');
+            return;
+        }
+
+        login(email, password);
+    });
+
+    function login(email, password) {
+        const validEmail = 'user@gmail.com';
+        const validPassword = '12345';
+    
+        // Proses login
+        if (email === validEmail && password === validPassword) {
+            // Berhasil login
+            alert('Login berhasil! Selamat datang!');
+            // Redirect ke halaman index.html
+            window.location.href = 'index.html';
+        } else {
+            // menampilkan pesan error jika login gagal
+            document.getElementById('errorMessage').style.display = 'block';
+        }
+    }
+})
